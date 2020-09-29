c64d37ad78359b77b84f56c36730c4038633b0bd
KongLinGui <18737010615@163.com>
2020-09-29 09:53:06 +0800

    迷你高尔夫球

---
+++ Assets/Exporter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class Exporter
+{
+	[MenuItem("Assets/ExportWithSettings")]
+	static void Export () {
+		AssetDatabase.ExportPackage("Assets", "Entire Project.unitypackage",ExportPackageOptions.Interactive | ExportPackageOptions.Recurse | ExportPackageOptions.IncludeLibraryAssets|ExportPackageOptions.IncludeDependencies    );
+	}
+}
